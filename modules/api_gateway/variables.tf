ad17b9dee2f6f501dd1e187b98955937cb3c6180
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-05-14 16:07:22 +0530

    Update variables.tf

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -1,12 +1,13 @@
-variable "api_gateway_stage_name" {
-  type = string
+variable "apigateway-stage-name" {
+  description = "The  Stage name."
+  type        = string
 }
 
 variable "lambda_invoke_arn" {
   type = string
 }
 
- 
+
 variable "lambda_function_name" {
   description = "The Lambda function name."
   type        = string
